8999e782c1e84f20e47144eeb714148b03ecc26f
sanjoy2436 <Sanjoyhalder8674@gmail.com>
2024-12-01 13:50:41 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,57 @@
+import ProductCard2 from "../components/ProductCard2"
+const Home = () => {
+  return (
+    <div className="grid grid-cols-4  justify-between items-center p-1  bg-green-500">
+      <ProductCard2
+      imgUrl='https://cdn.pixabay.com/photo/2020/04/30/04/32/indonesia-5111348_960_720.jpg'
+      title="DISCOVER YOUR'S ROAD JOURNEY "
+      />
+      <ProductCard2 
+      imgUrl='https://cdn.pixabay.com/photo/2022/05/06/17/47/bridge-7178826_960_720.jpg'
+      title="DISCOVER YOUR'S TRAVELLING SITE"
+      
+      />
+      <ProductCard2 
+      imgUrl='https://cdn.pixabay.com/photo/2022/07/24/14/12/path-7341731_960_720.jpg'
+      title='DISCOVER YOURS FOREST EXCITEMENT'
+      
+      />
+      <ProductCard2  
+       imgUrl="https://cdn.pixabay.com/photo/2018/02/19/00/14/tree-lined-path-3163913_960_720.jpg" 
+       title="LOVE YOUR'S WALKING">
+
+      </ProductCard2>
+   </div> 
+    
+    
+   
+    
+  )
+}
+
+export default Home
+  
+
+
+        
+    
+    
+      
+      
+  
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
